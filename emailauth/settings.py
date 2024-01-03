7f71f5258887c55d0c7ff46f5e937357acc9ec7d
Abelmekonn <123172904+Abelmekonn@users.noreply.github.com>
2024-01-03 23:36:21 +0300

    Update settings.py

--- emailauth/settings.py
+++ emailauth/settings.py
@@ -129,6 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
-EMAIL_HOST_USER = 'Abelmekonn9@gmail.com'  
-EMAIL_HOST_PASSWORD = 'uzesibdeivvyjuqt'  
+EMAIL_HOST_USER = 'Your Email'  
+EMAIL_HOST_PASSWORD = 'Password'  
 EMAIL_PORT = 587
